251ec417d91cad1cf549ca2ad44e878fe1374561
gseven-gdv <zerogravitystd@gmail.com>
2021-07-09 21:37:25 -0300

    Update Player.cs

--- Bubbles/Assets/Scripts/Player/Player.cs
+++ Bubbles/Assets/Scripts/Player/Player.cs
@@ -36,7 +36,6 @@ public class Player : MonoBehaviour
     [SerializeField] private float bottomColRadius = 0.5f;
 
     [HideInInspector] public bool canMove = true;
-    [HideInInspector] public bool isControlled = false;
     [HideInInspector] public bool shouldLerpMovement = false;
 
     public static Player I { get; private set; }
@@ -77,10 +76,6 @@ public class Player : MonoBehaviour
 
             wallJumped = false;
             shouldLerpMovement = false;
-        }
-
-        if (isControlled && InputManager.I.keyJump) {
-
         }
 
         if (canJump > 0 && InputManager.I.keyJump)

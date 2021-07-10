5d408bd878b2b07da5f703fb678968f953871f83
gseven-gdv <zerogravitystd@gmail.com>
2021-07-10 16:01:57 -0300

    Shuriken Graphics

---
+++ Bubbles/Assets/Scripts/Player/Shuriken.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Resources;
+
+public class Shuriken : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lookAngle = StaticRes.LookDir(transform.position);
+        Vector3 dir = StaticRes.LookDir(transform.position, true);
+
+        transform.Translate((dir).normalized * speed * Time.deltaTime);
+
+        //transform.localScale = lookAngle < 90 && lookAngle > -90 ? new Vector3(1f, 1f, 1f) : new Vector3(-1f, 1f, 1f);
+    }
+}
---
+++ Bubbles/Assets/Scripts/Player/ShurikenGraphics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShurikenGraphics : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(transform.forward * rotationSpeed * Time.deltaTime);
+    }
+}

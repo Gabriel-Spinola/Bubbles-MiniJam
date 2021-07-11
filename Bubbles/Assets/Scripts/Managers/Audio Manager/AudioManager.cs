5efa6eab797ac262e2e5496446410b492fb6cc9d
gseven-gdv <zerogravitystd@gmail.com>
2021-07-10 22:38:53 -0300

    Finish audio manager

--- Bubbles/Assets/Scripts/Managers/Audio Manager/AudioManager.cs
+++ Bubbles/Assets/Scripts/Managers/Audio Manager/AudioManager.cs
@@ -6,20 +6,39 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
+    public static AudioManager I;
+
     private void Awake()
     {
+        if (I == null) {
+            I = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         foreach (Sound sound in sounds) {
             sound.source = gameObject.AddComponent<AudioSource>();
 
             sound.source.clip = sound.clip;
             sound.source.volume = sound.volume;
             sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
         }
     }
 
     public void Play(string name)
     {
         Sound sound_ = Array.Find(sounds, sound => sound.name == name);
+
+        if (sounds == null) {
+            Debug.LogWarning($"Sound: { name } not found!");
+
+            return;
+        }
+
         sound_.source.Play();
     }
 }
